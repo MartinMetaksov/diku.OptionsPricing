db53b3a29af3840bc89fa9b2114c96d880b2934f
Marek Hlava <headsvk@gmail.com>
2018-03-23 13:22:26 +0100

    simple allocation of global arrays

--- cuda-option/CudaOption.cu
+++ cuda-option/CudaOption.cu
@@ -16,22 +16,43 @@ using namespace chrono;
 __global__ void
 computeSingleOptionKernel(real *res, OptionConstants *options, real *QsAll, real *QsCopyAll, real *alphasAll, int *QsInd, int *alphasInd)
 {
-    auto c = options[threadIdx.x];
-    auto Qs = QsAll + QsInd[threadIdx.x];
-    auto QsCopy = QsCopyAll + QsInd[threadIdx.x];
-    auto alphas = alphasAll + alphasInd[threadIdx.x];
+    auto c = options[blockIdx.x];
+    auto Qs = QsAll + QsInd[blockIdx.x];
+    auto QsCopy = QsCopyAll + QsInd[blockIdx.x];
+    auto alphas = alphasAll + alphasInd[blockIdx.x];
 
     // some test result
-    res[threadIdx.x] = c.n;
+    res[blockIdx.x] = c.n;
 }
 
 void computeCuda(OptionConstants *options, real *result, int count, bool isTest = false)
 {
-    auto blockSize = 1;
+    // Compute indices
+    int* QsInd = new int[count];
+    int* alphasInd = new int[count];
+    QsInd[0] = 0;
+    alphasInd[0] = 0;
+    int totalQsCount = 0;
+    int totalAlphasCount = 0;
+    for (auto i = 0; i < count - 1; ++i)
+    {
+        auto &option = options[i];
+        totalQsCount += option.width;
+        totalAlphasCount += option.n + 1;
+        QsInd[i + 1] = totalQsCount;
+        alphasInd[i + 1] = totalAlphasCount;
+    }
+    totalQsCount += options[count - 1].width;
+    totalAlphasCount += options[count - 1].n + 1;
     
+    auto blockSize = 1;
+
     if (isTest)
     {
+        int memorySize = count * sizeof(real) + count * sizeof(OptionConstants) + 2 * count * sizeof(int)
+                        + 2 * totalQsCount * sizeof(real) + totalAlphasCount * sizeof(real);
         cout << "Running trinomial option pricing for " << count << " options with block size " << blockSize << endl;
+        cout << "Global memory size " << memorySize << endl;
     }
 
     auto time_begin = steady_clock::now();
@@ -41,8 +62,15 @@ void computeCuda(OptionConstants *options, real *result, int count, bool isTest 
     OptionConstants *d_options;
     CudaSafeCall(cudaMalloc((void **)&d_result, count * sizeof(real)));
     CudaSafeCall(cudaMalloc((void **)&d_options, count * sizeof(OptionConstants)));
+    CudaSafeCall(cudaMalloc((void **)&d_QsInd, count * sizeof(int)));
+    CudaSafeCall(cudaMalloc((void **)&d_alphasInd, count * sizeof(int)));
+    CudaSafeCall(cudaMalloc((void **)&d_Qs, totalQsCount * sizeof(real)));
+    CudaSafeCall(cudaMalloc((void **)&d_QsCopy, totalQsCount * sizeof(real)));
+    CudaSafeCall(cudaMalloc((void **)&d_alphas, totalAlphasCount * sizeof(real)));
 
     cudaMemcpy(d_options, options, count * sizeof(OptionConstants), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_QsInd, QsInd, count * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_alphasInd, alphasInd, count * sizeof(int), cudaMemcpyHostToDevice);
 
     auto time_begin_kernel = steady_clock::now();
     computeSingleOptionKernel<<<count, blockSize>>>(d_result, d_options, d_Qs, d_QsCopy, d_alphas, d_QsInd, d_alphasInd);
