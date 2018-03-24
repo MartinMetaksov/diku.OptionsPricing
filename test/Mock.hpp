3a0cb36ef790ed0bfeea2240d0b7092fe009229f
Marek Hlava <headsvk@gmail.com>
2018-03-24 17:31:42 +0100

    added mocking support, cuda error reporting fix

---
+++ test/Mock.hpp
@@ -0,0 +1,24 @@
+#ifndef MOCK_HPP
+#define MOCK_HPP
+
+#include "../common/OptionConstants.hpp"
+
+class Mock
+{
+  public:
+    static void mockConstants(OptionConstants *result, int count, int width, int steps)
+    {
+        OptionConstants c;
+        c.n = steps;
+        c.dt = steps / 36.0;
+        c.t = steps / 12;
+        c.X = 60;
+        c.dr = 0.1;
+        c.jmax = width / 2;
+        c.M = minus184 / (c.jmax - 1);
+        c.width = 2 * c.jmax + 1;
+        fill_n(result, count, c);
+    }
+};
+
+#endif
