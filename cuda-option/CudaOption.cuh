e767702aea7faa694b176151ba827f1bca7ba01c
Martin Metaksov <info@metaksov.dk>
2018-03-26 00:29:27 +0200

    almost done with kernel for one option per thread

--- cuda-option/CudaOption.cuh
+++ cuda-option/CudaOption.cuh
@@ -9,6 +9,8 @@
 #include "../cuda/CudaErrors.cuh"
 #include <cuda_runtime.h>
 #include <chrono>
+#include <thrust/fill.h>
+#include <thrust/execution_policy.h>
 
 using namespace chrono;
 using namespace trinom;
@@ -29,8 +31,160 @@ computeSingleOptionKernel(real *res, OptionConstants *options, real *QsAll, real
     auto QsCopy = QsCopyAll + QsInd[idx];
     auto alphas = alphasAll + alphasInd[idx];
 
-    // some test result
-    res[idx] = c.n;
+    /*
+     * TODO: I'll remove all the jvalues logic tomorrow and calculate it every time we need it instead of storing it.
+     */
+    auto jvalues = new real[c.width * 4];
+    auto jmin = -c.jmax;
+
+    for (auto i = 0; i < c.width * 4; i+=4)
+    {
+        if (i == 0) {
+            jvalues[i] = jmin * c.dr;
+            jvalues[i+1] = PU_B(jmin, c.M);
+            jvalues[i+2] = PM_B(jmin, c.M);
+            jvalues[i+3] = PD_B(jmin, c.M);
+        } 
+        else if (i == c.width*4 - 4) {
+            jvalues[i] = c.jmax * c.dr;
+            jvalues[i+1] = PU_C(c.jmax, c.M);
+            jvalues[i+2] = PM_C(c.jmax, c.M);
+            jvalues[i+3] = PD_C(c.jmax, c.M);
+        } 
+        else {
+            auto j = (i/4) + jmin;
+            jvalues[i] = j * c.dr;
+            jvalues[i+1] = PU_A(j, c.M);
+            jvalues[i+2] = PM_A(j, c.M);
+            jvalues[i+3] = PD_A(j, c.M);
+        }
+    }
+
+    Qs[c.jmax] = one;
+    alphas[0] = getYieldAtYear(c.dt);
+
+    for (auto i = 0; i < c.n; ++i)
+    {
+        auto jhigh = min(i, c.jmax);
+        auto alpha = alphas[i];
+
+        // Forward iteration step, compute Qs in the next time step
+        for (auto j = -jhigh; j <= jhigh; ++j)
+        {
+            auto jind = j - jmin;      // array index for j
+            auto rate = jvalues[jind * 4]; // precomputed rates
+            auto pu = jvalues[jind * 4 + 1]; // precomputed up probability
+            auto pm = jvalues[jind * 4 + 2]; // precomputed mid probability
+            auto pd = jvalues[jind * 4 + 3]; // precomputed down probability
+
+            auto qexp = Qs[jind] * exp(-(alpha + rate) * c.dt);
+
+            if (j == jmin)
+            {
+                // Bottom edge branching
+                QsCopy[jind + 2] += pu * qexp; // up two
+                QsCopy[jind + 1] += pm * qexp; // up one
+                QsCopy[jind] += pd * qexp;     // middle
+            }
+            else if (j == c.jmax)
+            {
+                // Top edge branching
+                QsCopy[jind] += pu * qexp;     // middle
+                QsCopy[jind - 1] += pm * qexp; // down one
+                QsCopy[jind - 2] += pd * qexp; // down two
+            }
+            else
+            {
+                // Standard branching
+                QsCopy[jind + 1] += pu * qexp; // up
+                QsCopy[jind] += pm * qexp;     // middle
+                QsCopy[jind - 1] += pd * qexp; // down
+            }
+        }
+
+        // Determine the new alpha using equation 30.22
+        // by summing up Qs from the next time step
+        auto jhigh1 = min(i + 1, c.jmax);
+        real alpha_val = 0;
+        for (auto j = -jhigh1; j <= jhigh1; ++j)
+        {
+            auto jind = j - jmin;      // array index for j
+            // auto jval = jvalues[jind]; // precomputed probabilities and rates
+            auto rate = jvalues[jind * 4]; // precomputed rates
+            alpha_val += QsCopy[jind] * exp(-rate * c.dt);
+        }
+
+        alphas[i + 1] = computeAlpha(alpha_val, i, c.dt);
+
+        // Switch Qs
+        auto QsT = Qs;
+        Qs = QsCopy;
+        QsCopy = QsT;
+        thrust::fill_n(thrust::device, QsCopy, c.width, 0);
+    }
+    
+    // Backward propagation
+    auto call = Qs; // call[j]
+    auto callCopy = QsCopy;
+
+    thrust::fill_n(thrust::device, call, c.width, 100); // initialize to 100$
+
+    for (auto i = c.n - 1; i >= 0; --i)
+    {
+        auto jhigh = min(i, c.jmax);
+        auto alpha = alphas[i];
+        auto isMaturity = i == ((int)(c.t / c.dt));
+
+        for (auto j = -jhigh; j <= jhigh; ++j)
+        {
+            auto jind = j - jmin;      // array index for j
+            
+            auto rate = jvalues[jind * 4]; // precomputed rates
+            auto pu = jvalues[jind * 4 + 1]; // precomputed up probability
+            auto pm = jvalues[jind * 4 + 2]; // precomputed mid probability
+            auto pd = jvalues[jind * 4 + 3]; // precomputed down probability
+
+            auto callExp = exp(-(alpha + rate) * c.dt);
+
+            real res;
+            if (j == c.jmax)
+            {
+                // Top edge branching
+                res = (pu * call[jind] +
+                       pm * call[jind - 1] +
+                       pd * call[jind - 2]) *
+                      callExp;
+            }
+            else if (j == jmin)
+            {
+                // Bottom edge branching
+                res = (pu * call[jind + 2] +
+                       pm * call[jind + 1] +
+                       pd * call[jind]) *
+                      callExp;
+            }
+            else
+            {
+                // Standard branching
+                res = (pu * call[jind + 1] +
+                       pm * call[jind] +
+                       pd * call[jind - 1]) *
+                      callExp;
+            }
+
+            // after obtaining the result from (i+1) nodes, set the call for ith node
+            callCopy[jind] = isMaturity ? max(c.X - res, zero) : res;
+        }
+
+        // Switch call arrays
+        auto callT = call;
+        call = callCopy;
+        callCopy = callT;
+
+        thrust::fill_n(thrust::device, callCopy, c.width, 0);
+    }
+
+    res[idx] = call[c.jmax];
 }
 
 void computeOptions(OptionConstants *options, real *result, int count, bool isTest = false)
