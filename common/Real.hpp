6da3e0d6b714efe690d1ea13009ccf6bb221d5ff
Marek Hlava <headsvk@gmail.com>
2018-06-21 11:48:58 +0200

    fix for floats

--- common/Real.hpp
+++ common/Real.hpp
@@ -14,25 +14,16 @@ typedef float real;
 #define ROUND(X) roundf((X))
 #endif
 
-#define zero 0.0
-#define one 1.0
-#define two 2.0
-#define half 0.5
-#define three 3.0
-#define six 6.0
-#define seven 7.0
-#define year 365.0
-#define minus184 -0.184
+#define zero (real)0.0
+#define one (real)1.0
+#define two (real)2.0
+#define half (real)0.5
+#define three (real)3.0
+#define six (real)6.0
+#define seven (real)7.0
+#define year (real)365.0
+#define minus184 (real)-0.184
 
-//const real zero = 0;
-//const real one = 1;
-//const real two = 2;
-//const real half = one / two;
-//const real three = 3;
-//const real six = 6;
-//const real seven = 7;
-//const real year = 365;
-//const real minus184 = -0.184;
 } // namespace trinom
 
 #endif
