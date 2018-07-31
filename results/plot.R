a8093bec9567545f8edc46f5e5700e905240b9ca
Marek Hlava <headsvk@gmail.com>
2018-08-01 00:52:59 +0200

    nice filename titles in plots, added new plots

--- results/plot.R
+++ results/plot.R
@@ -22,7 +22,7 @@ readFuthark <- function(file, real) {
 }
 
 title = "results"
-datasets.names = c("Cuda-option", "Cuda-multi", "Futhark-basic", "Futhark-flat")
+datasets.names = c("CUDA-option", "CUDA-multi", "Futhark-basic", "Futhark-flat")
 datasets.files = c("option.csv", "multi.csv", "basic32.json", "basic64.json", "flat32.json", "flat64.json")
 
 # load and join data
@@ -135,25 +135,39 @@ plotScatter <- function() {
 }
 
 mapSort <- function(d) {
-  mapvalues(d, from=c("-","w","W", "h", "H"), to=c("None", "Width ▲", "Width ▼", "Height ▲","Height ▼"))
+  mapvalues(d, from=c("-","w","W", "h", "H"), to=c("None ", "Width ▲", "Width ▼", "Height ▲","Height ▼"))
+}
+
+mapFilesTitle <- function(d) {
+  mapvalues(d, from=c("0_UNIFORM", "1_RAND", "2_RANDCONSTHEIGHT", "3_RANDCONSTWIDTH", "4_SKEWED", "5_SKEWEDCONSTHEIGHT", "6_SKEWEDCONSTWIDTH"),
+            to=c("Dataset 0 - Uniform", "Dataset 1 - Random", "Dataset 2 - Random Constant Height", "Dataset 3 - Random Constant Width", "Dataset 4 - Skewed", "Dataset 5 - Skewed Constant Height", "Dataset 6 - Skewed Constant Width"))
+}
+
+mapFilesAxis <- function(d) {
+  mapvalues(d, from=c("0_UNIFORM", "1_RAND", "2_RANDCONSTHEIGHT", "3_RANDCONSTWIDTH", "4_SKEWED", "5_SKEWEDCONSTHEIGHT", "6_SKEWEDCONSTWIDTH"),
+            to=c("0 Uniform ", "1 Random ", "2 Random \nconst height ", "3 Random \nconst width ", "4 Skewed ", "5 Skewed \nconst height ", "6 Skewed \nconst width "))
 }
 
 makeBarPlot <- function(d, y, y_title, x_title, name, y_order = NA) {
   d$float <- round(d$float, 3)
   d$double <- round(d$double, 3)
+  d$fileTitle <- mapFilesTitle(d$file)
   files <- split(d, d$file)
+  
   for (i in seq_along(files)) {
     title <- names(files)[i]
+    plotTitle <- files[[i]]$fileTitle
     p <-
       plot_ly(files[[i]]) %>%
-      add_trace(x = ~float, y = y, type = 'bar', name = "float", text = ~float, textposition = "auto",
+      add_trace(x = ~float, y = y, type = 'bar', name = "Float", text = ~float, textposition = "auto",
                 marker = list(color = 'rgb(239,138,98)',
                               line = list(color = 'rgb(8,48,107)', width = 0.7))) %>%
-      add_trace(x = ~double, y = y, type = 'bar', name = "double", text = ~double, textposition = "auto",
+      add_trace(x = ~double, y = y, type = 'bar', name = "Double", text = ~double, textposition = "auto",
                 marker = list(color = 'rgb(103,169,207)',
                               line = list(color = 'rgb(8,48,107)', width = 0.7))) %>%
-      layout(title = title,
+      layout(title = plotTitle,
              barmode = 'group',
+             autosize = F,
              margin = list(t = 120),
              font = list(family = "sans serif", size = 60),
              xaxis = list(title = x_title),
@@ -163,16 +177,48 @@ makeBarPlot <- function(d, y, y_title, x_title, name, y_order = NA) {
   }
 }
 
+makeBarPlotTypes <- function(d, title, name) {
+  x_title <- "Time (sec)"
+  y_title <- "Dataset"
+  colnames(d) <- sub("-", ".", colnames(d), fixed = TRUE)
+  d$file <- mapFilesAxis(d$file)
+  
+  p <-
+    plot_ly(d) %>%
+    add_trace(x = ~CUDA.option, y = ~file, type = 'bar', name = "CUDA-option", text = ~CUDA.option, textposition = "auto",
+              marker = list(color = 'rgb(102,194,165',
+                            line = list(color = 'rgb(8,48,107)', width = 0.7))) %>%
+    add_trace(x = ~CUDA.multi, y = ~file, type = 'bar', name = "CUDA-multi", text = ~CUDA.multi, textposition = "auto",
+              marker = list(color = 'rgb(252,141,98)',
+                            line = list(color = 'rgb(8,48,107)', width = 0.7))) %>%
+    add_trace(x = ~Futhark.basic, y = ~file, type = 'bar', name = "Futhark-basic", text = ~Futhark.basic, textposition = "auto",
+              marker = list(color = 'rgb(141,160,203)',
+                            line = list(color = 'rgb(8,48,107)', width = 0.7))) %>%
+    add_trace(x = ~Futhark.flat, y = ~file, type = 'bar', name = "Futhark-flat", text = ~Futhark.flat, textposition = "auto",
+              marker = list(color = 'rgb(231,138,195)',
+                            line = list(color = 'rgb(8,48,107)', width = 0.7))) %>%
+    layout(title = title,
+           barmode = 'group',
+           margin = list(t = 120),
+           font = list(family = "sans serif", size = 60),
+           xaxis = list(title = x_title),
+           yaxis = list(title = y_title, autorange = "reversed"),
+           legend = list(orientation = 'h'))
+  orca(p, paste(name, ".png", sep = ""), width = 2300, height = 2800)
+}
+
 # CUDA-option
 plotCudaOptionBlocks <- function() {
   subset <- data[data$type == datasets.names[1], c(1,2,5,8)]
   cast <- dcast(subset, file + block ~ precision, value.var = "total.time", min)
+  cast$block <- paste(cast$block, " ", sep = "")
   makeBarPlot(cast, ~block, "Block size", "Time (sec)", "option-blocks")
 }
 
 plotCudaOptionVersions <- function() {
   subset <- data[data$type == datasets.names[1], c(1,2,4,8)]
   cast <- dcast(subset, file + version ~ precision, value.var = "total.time", min)
+  cast$version <- paste(cast$version, " ", sep = "")
   makeBarPlot(cast, ~version, "Version", "Time (sec)", "option-versions")
 }
 
@@ -180,12 +226,14 @@ plotCudaOptionSorts <- function() {
   subset <- data[data$type == datasets.names[1], c(1,2,6,8)]
   subset$sort <- mapSort(subset$sort)
   cast <- dcast(subset, file + sort ~ precision, value.var = "total.time", min)
+  cast$sort <- paste(cast$sort, " ", sep = "")
   makeBarPlot(cast, ~sort, "Sorting", "Time (sec)", "option-sorts")
 }
 
 plotCudaOptionVersionsMem <- function() {
   subset <- data[data$type == datasets.names[1], c(1,2,4,9)]
   cast <- dcast(subset, file + version ~ precision, value.var = "memory", min)
+  cast$version <- paste(cast$version, " ", sep = "")
   makeBarPlot(cast, ~version, "Version", "Memory (MB)", "mem-option-versions")
 }
 
@@ -193,6 +241,7 @@ plotCudaOptionVersionsMem <- function() {
 plotCudaMultiVersions <- function() {
   subset <- data[data$type == datasets.names[2], c(1,2,4,8)]
   cast <- dcast(subset, file + version ~ precision, value.var = "total.time", min)
+  cast$version <- paste(cast$version, " ", sep = "")
   makeBarPlot(cast, ~version, "Version", "Time (sec)", "multi-versions")
 }
 
@@ -200,18 +249,31 @@ plotCudaMultiSorts <- function() {
   subset <- data[data$type == datasets.names[2], c(1,2,6,8)]
   subset$sort <- mapSort(subset$sort)
   cast <- dcast(subset, file + sort ~ precision, value.var = "total.time", min)
+  cast$sort <- paste(cast$sort, " ", sep = "")
   makeBarPlot(cast, ~sort, "Sorting", "Time (sec)", "multi-sorts")
 }
 
 plotCudaMultiVersionsMem <- function() {
   subset <- data[data$type == datasets.names[2], c(1,2,4,9)]
   cast <- dcast(subset, file + version ~ precision, value.var = "memory", min)
+  cast$version <- paste(cast$version, " ", sep = "")
   makeBarPlot(cast, ~version, "Version", "Memory (MB)", "mem-multi-versions")
 }
 
 plotTypes <- function() {
   subset <- data[, c(1,2,8,10)]
   cast <- dcast(subset, file + type ~ precision, value.var = "total.time", min)
+  
+  cast$float <- round(cast$float, 3)
+  cast$double <- round(cast$double, 3)
+  
+  float <- dcast(cast[, -3], file ~ type)
+  double <- dcast(cast[, -4], file ~ type)
+  
+  makeBarPlotTypes(float, "Parallel Implementations - Float", "all-approaches-float")
+  makeBarPlotTypes(double, "Parallel Implementations - Double", "all-approaches-double")
+  
+  cast$type <- paste(cast$type, " ", sep = "")
   makeBarPlot(cast, ~type, "Parallel implementation", "Time (sec)", "all-approaches", datasets.names)
 }
 
